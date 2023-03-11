50372336a54e507d19123b0496d8d4780a9b80c6
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-11 17:51:09 +0530

    ADDED : code for bst, add and getSize

--- src/main/MyBinaryNode.java
+++ src/main/MyBinaryNode.java
@@ -1,4 +1,14 @@
 package main;
 
-public class MyBinaryNode {
+public class MyBinaryNode<K extends Comparable<K>> {
+
+    K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key){
+        this.key=key;
+        this.left=null;
+        this.right=null;
+    }
 }
--- src/test/MyBinaryTreeTest.java
+++ src/test/MyBinaryTreeTest.java
@@ -1,4 +1,18 @@
 package test;
 
+import main.MyBinaryTree;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MyBinaryTreeTest {
+
+    @Test
+    public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree(){
+        MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size= myBinaryTree.getSize();
+        Assert.assertEquals(3, size);
+    }
 }
